66c1dacbb04a5eea61df3037a9541d2326599ed4
matt-colen <mzcolen@gmail.com>
2023-09-17 14:49:20 -0500

    adds copy-on-click to the passwords btns

--- index.js
+++ index.js
@@ -182,8 +182,14 @@ const generatePasswords = (arr) => {
 
 // Renders passwords
 const renderPasswords = (arr) => {
+  const copyIcon = `<svg width="25" height="25" viewBox="0 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <path d="M20.5 9.5H11.5C10.3954 9.5 9.5 10.3954 9.5 11.5V20.5C9.5 21.6046 10.3954 22.5 11.5 22.5H20.5C21.6046 22.5 22.5 21.6046 22.5 20.5V11.5C22.5 10.3954 21.6046 9.5 20.5 9.5Z" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+  <path d="M5.5 15.5H4.5C3.96957 15.5 3.46086 15.2893 3.08579 14.9142C2.71071 14.5391 2.5 14.0304 2.5 13.5V4.5C2.5 3.96957 2.71071 3.46086 3.08579 3.08579C3.46086 2.71071 3.96957 2.5 4.5 2.5H13.5C14.0304 2.5 14.5391 2.71071 14.9142 3.08579C15.2893 3.46086 15.5 3.96957 15.5 4.5V5.5" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+  </svg>`;
   document.querySelector("#password1").textContent = arr[0].join("");
   document.querySelector("#password2").textContent = arr[1].join("");
+  document.querySelector("#password1").innerHTML += copyIcon;
+  document.querySelector("#password2").innerHTML += copyIcon;
 };
 
 document.querySelector("#generate-btn").addEventListener("click", () => {
@@ -197,3 +203,16 @@ document.querySelector("#passwordLength").addEventListener("keydown", (e) => {
     setPasswordCharacters();
   }
 });
+
+// Adds ability to copy-on-click
+const copyOnClick = () => {
+  const copyBtns = document.querySelectorAll(".password-btn");
+  copyBtns.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      const textToCopy = btn.textContent;
+      navigator.clipboard.writeText(textToCopy);
+    });
+  });
+};
+
+document.addEventListener("click", copyOnClick);
