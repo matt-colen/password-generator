4e26c633b296aff7ed420cd2c7f01bc5e8036ce7
matt-colen <mzcolen@gmail.com>
2023-09-17 12:25:21 -0500

    Adds toggle btns and light/dark mode functionality
    Adds toggle btns to HTML, light/dark mode toggle function to JS, & button styling.

--- index.js
+++ index.js
@@ -1,3 +1,46 @@
+// Dark/Light Mode Switch
+
+let lightMode = false;
+
+// Function to toggle the modes
+const toggleMode = () => {
+  // This string will be used to find all DOM elements that include the string in their className
+  const targetString = lightMode ? "--light" : "--dark";
+  // This string will be used to replace the targetString in the classNames of all matching elements
+  const replacementString = !lightMode ? "--light" : "--dark";
+  // Selects all elements with classNames containing the target string
+  const targetElements = document.querySelectorAll(
+    `[class*="${targetString}"]`
+  );
+
+  // Loops through the selected elements
+  targetElements.forEach((element) => {
+    // Gets the current class name for the element
+    const currentClassName = element.className;
+    // Creates a new class name by replacing the matching portion
+    const newClassName = currentClassName.replace(
+      new RegExp(targetString, "g"),
+      replacementString
+    );
+
+    // Updates the element's className attribute
+    element.className = newClassName;
+  });
+  updateMode();
+};
+
+// Function that sets the current mode
+const updateMode = () => {
+  lightMode ? (lightMode = false) : (lightMode = true);
+};
+
+// Adds event listener for mode toggle button
+document.querySelector("#modeToggleSwitch").addEventListener("click", () => {
+  toggleMode();
+});
+
+// Random Password Generator
+
 const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
 const alphaUppercase = [
@@ -89,3 +132,5 @@ const symbols = [
   "?",
   "/",
 ];
+
+const generatePasswords = () => {};
