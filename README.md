5d88f107ed70955c2c6c9715b2108c1311d58c5a
matt-colen <mzcolen@gmail.com>
2023-09-23 15:01:47 -0500

    refactor of class & id names
    Refactored class & id names, and tried to improve organization and commenting throughout.

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
-# random-password-generator
+# Random Password Generator
 
-Random password generator project
+## Cool features of this project:
+
+- Allows the user to toggle between a light and dark mode.
+- Allows the user to toggle whether they want to include symbols and/or numbers in the password.
+- Allows the user to set the desired length of the password.
+- Generates 2 random passwords on click matching the provided criteria.
+- Allows the user to copy either of the provided passwords.
--- index.js
+++ index.js
@@ -35,7 +35,7 @@ const updateMode = () => {
 };
 
 // Adds event listener for mode toggle button
-document.querySelector("#modeToggleSwitch").addEventListener("click", () => {
+document.querySelector("#mode-toggle").addEventListener("click", () => {
   toggleMode();
 });
 
@@ -133,19 +133,19 @@ const symbols = [
   "/",
 ];
 
-let useSymbols = document.querySelector("#symbolsToggleSwitch").checked;
-let useNumbers = document.querySelector("#numbersToggleSwitch").checked;
+let useSymbols = document.querySelector("#symbols-toggle").checked;
+let useNumbers = document.querySelector("#numbers-toggle").checked;
 
 const updatePasswordOptions = () => {
-  useSymbols = document.querySelector("#symbolsToggleSwitch").checked;
-  useNumbers = document.querySelector("#numbersToggleSwitch").checked;
+  useSymbols = document.querySelector("#symbols-toggle").checked;
+  useNumbers = document.querySelector("#numbers-toggle").checked;
 };
 
-document.querySelector("#symbolsToggleSwitch").addEventListener("click", () => {
+document.querySelector("#symbols-toggle").addEventListener("click", () => {
   updatePasswordOptions();
 });
 
-document.querySelector("#numbersToggleSwitch").addEventListener("click", () => {
+document.querySelector("#numbers-toggle").addEventListener("click", () => {
   updatePasswordOptions();
 });
 
@@ -167,7 +167,7 @@ const setPasswordCharacters = () => {
 
 // Generates 2 random passwords
 const generatePasswords = (arr) => {
-  const passwordLength = document.querySelector("#passwordLength").value;
+  const passwordLength = document.querySelector("#password-length").value;
   const password1 = [];
   const password2 = [];
   const passwords = [password1, password2];
@@ -182,14 +182,14 @@ const generatePasswords = (arr) => {
 
 // Renders passwords
 const renderPasswords = (arr) => {
-  const copyIcon = `<svg width="25" height="25" viewBox="0 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+  const copyIcon = `<svg class="click-disabled" width="25" height="25" viewBox="0 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg">
   <path d="M20.5 9.5H11.5C10.3954 9.5 9.5 10.3954 9.5 11.5V20.5C9.5 21.6046 10.3954 22.5 11.5 22.5H20.5C21.6046 22.5 22.5 21.6046 22.5 20.5V11.5C22.5 10.3954 21.6046 9.5 20.5 9.5Z" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
   <path d="M5.5 15.5H4.5C3.96957 15.5 3.46086 15.2893 3.08579 14.9142C2.71071 14.5391 2.5 14.0304 2.5 13.5V4.5C2.5 3.96957 2.71071 3.46086 3.08579 3.08579C3.46086 2.71071 3.96957 2.5 4.5 2.5H13.5C14.0304 2.5 14.5391 2.71071 14.9142 3.08579C15.2893 3.46086 15.5 3.96957 15.5 4.5V5.5" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
   </svg>`;
-  document.querySelector("#password1").textContent = arr[0].join("");
-  document.querySelector("#password2").textContent = arr[1].join("");
-  document.querySelector("#password1").innerHTML += copyIcon;
-  document.querySelector("#password2").innerHTML += copyIcon;
+  document.querySelector("#password-1").textContent = arr[0].join("");
+  document.querySelector("#password-2").textContent = arr[1].join("");
+  document.querySelector("#password-1").innerHTML += copyIcon;
+  document.querySelector("#password-2").innerHTML += copyIcon;
 };
 
 document.querySelector("#generate-btn").addEventListener("click", () => {
@@ -197,22 +197,30 @@ document.querySelector("#generate-btn").addEventListener("click", () => {
 });
 
 // Adds ability to generate passwords when the user presses the "Enter" key
-document.querySelector("#passwordLength").addEventListener("keydown", (e) => {
+document.querySelector("#password-length").addEventListener("keydown", (e) => {
   if (e.keyCode === 13) {
     e.preventDefault();
     setPasswordCharacters();
   }
 });
 
-// Adds ability to copy-on-click
-const copyOnClick = () => {
-  const copyBtns = document.querySelectorAll(".password-btn");
-  copyBtns.forEach((btn) => {
-    btn.addEventListener("click", () => {
-      const textToCopy = btn.textContent;
-      navigator.clipboard.writeText(textToCopy);
-    });
+// Adds event listeners for the password buttons
+document.querySelectorAll(".password-btn").forEach((btn) => {
+  btn.addEventListener("click", () => {
+    copyOnClick(btn);
   });
+});
+
+// Copies clicked password to clipboard
+const copyOnClick = (btn) => {
+  const textToCopy = btn.textContent;
+  navigator.clipboard.writeText(textToCopy);
+  showCopied(btn);
 };
 
-document.addEventListener("click", copyOnClick);
+// Adds styling to clicked password button
+const showCopied = (clickedBtn) => {
+  clickedBtn.classList.toggle("password-btn--copied");
+  clickedBtn.textContent = "Copied";
+  clickedBtn.disabled = true;
+};
