4b83060f208e6ba32d18b9a7bcafdf68529a36a8
matt-colen <mzcolen@gmail.com>
2023-09-24 14:52:26 -0500

    Adds copy-on-click feature

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import * as characters from "./characters.js";
 // DARK/LIGHT MODE TOGGLE
 let lightMode = false;
 
+// Function to toggle light/dark mode
 const toggleMode = () => {
   const targetString = lightMode ? "--light" : "--dark"; // Determines the class substring needing replaced
   const replacementString = !lightMode ? "--light" : "--dark"; // Determines the replacement class substring
@@ -19,14 +20,13 @@ const toggleMode = () => {
       new RegExp(targetString, "g"),
       replacementString
     );
-
     // Updates the element's className attribute
     element.className = newClassName;
   });
   updateMode();
 };
 
-// Function that sets the current state of the mode
+// Function that updates the mode's state
 const updateMode = () => {
   lightMode ? (lightMode = false) : (lightMode = true);
 };
@@ -37,29 +37,30 @@ document.querySelector("#mode-toggle").addEventListener("click", () => {
 });
 
 // HANDLING THE PASSWORD OPTIONS
+let passwordsGenerated = false;
 
 // Global variables tracking the password options
-let useSymbols = document.querySelector("#symbols-toggle").checked;
-let useNumbers = document.querySelector("#numbers-toggle").checked;
+let useSymbols = true;
+let useNumbers = true;
 
 // Function that updates the state of the password options
-const updatePasswordOptions = () => {
-  useSymbols = document.querySelector("#symbols-toggle").checked;
-  useNumbers = document.querySelector("#numbers-toggle").checked;
+const updatePasswordOptions = (e) => {
+  e.target.id === "symbols-toggle"
+    ? (useSymbols = !useSymbols)
+    : (useNumbers = !useNumbers);
 };
 
 // Event listeners for the symbol/number toggle buttons
-document.querySelector("#symbols-toggle").addEventListener("click", () => {
-  updatePasswordOptions();
-});
-document.querySelector("#numbers-toggle").addEventListener("click", () => {
-  updatePasswordOptions();
+document.querySelectorAll(".toggle--options").forEach((elem) => {
+  elem.addEventListener("click", (e) => {
+    updatePasswordOptions(e);
+  });
 });
 
 // Function that sets the password character options depending on the toggle switch selections
 const setPasswordCharacters = () => {
   // Initial setup of password character options
-  let passwordCharacters = [];
+  const passwordCharacters = [];
   // Pushes all the alpha characters to the array
   characters.alphaLowercase.forEach((letter) =>
     passwordCharacters.push(letter)
@@ -72,9 +73,9 @@ const setPasswordCharacters = () => {
     characters.symbols.forEach((symbol) => passwordCharacters.push(symbol));
     characters.numbers.forEach((number) => passwordCharacters.push(number));
   } else if (useSymbols && !useNumbers) {
-    symbols.forEach((symbol) => passwordCharacters.push(symbol));
+    characters.symbols.forEach((symbol) => passwordCharacters.push(symbol));
   } else if (!useSymbols && useNumbers) {
-    numbers.forEach((number) => passwordCharacters.push(number));
+    characters.numbers.forEach((number) => passwordCharacters.push(number));
   }
   generatePasswords(passwordCharacters);
 };
@@ -84,9 +85,8 @@ let password1;
 let password2;
 
 // Function that generates 2 random passwords
-
 const generatePasswords = (arr) => {
-  const passwordLength = document.querySelector("#password-length").value;
+  const passwordLength = document.querySelector("#password-length").value; // Sets desired password length
   const password1 = [];
   const password2 = [];
   const passwords = [password1, password2];
@@ -106,19 +106,26 @@ const generatePasswords = (arr) => {
 const renderPasswords = (arr) => {
   // Adds a new copy icon
   const copyIcon = characters.copyIcon;
+  const password1El = document.querySelector("#password-1");
+  const password2El = document.querySelector("#password-2");
+  // Updating global variable to current pw values
   password1 = arr[0].join("");
   password2 = arr[1].join("");
-  document.querySelector("#password-1").textContent = password1;
-  document.querySelector("#password-2").textContent = password2;
-  document.querySelector("#password-1").innerHTML += copyIcon;
-  document.querySelector("#password-2").innerHTML += copyIcon;
+  // Updating the text content for the current pw values
+  password1El.textContent = password1;
+  password2El.textContent = password2;
+  // Adds the copy icon to the new passwords
+  password1El.innerHTML += copyIcon;
+  password2El.innerHTML += copyIcon;
+  // Updates state of password generation
+  passwordsGenerated = true;
 };
 
-// Adds event listeners for generate btn and enter key on length of password field
+// Adds event listener for the generate pw btn
 document.querySelector("#generate-btn").addEventListener("click", () => {
   setPasswordCharacters();
 });
-// Adds ability to generate passwords when the user presses the "Enter" key
+// Adds event listener for the "Enter" key on the num input field
 document.querySelector("#password-length").addEventListener("keydown", (e) => {
   if (e.keyCode === 13) {
     e.preventDefault();
@@ -128,20 +135,37 @@ document.querySelector("#password-length").addEventListener("keydown", (e) => {
 
 // COPY ON CLICK FEATURE
 
-// Copies clicked password to clipboard
+// Copies clicked password to the clipboard
 const copyOnClick = (btn) => {
-  const textToCopy = btn.textContent;
-  navigator.clipboard.writeText(textToCopy);
-  showCopied(btn);
+  if (passwordsGenerated) {
+    const textToCopy = btn.textContent;
+    navigator.clipboard.writeText(textToCopy);
+    showCopied(btn);
+  }
 };
 
 // Adds styling to clicked password button
 const showCopied = (clickedBtn) => {
-  if (clickedBtn.id === "password-1") {
-  }
   clickedBtn.classList.add("password-btn--copied");
+  clickedBtn.id === "password-1"
+    ? (password1 = clickedBtn.textContent)
+    : (password2 = clickedBtn.textContent);
   clickedBtn.textContent = "Copied";
   clickedBtn.disabled = true;
+  restoreOtherPassword(clickedBtn);
+};
+
+const restoreOtherPassword = (clickedBtn) => {
+  const copyIcon = characters.copyIcon;
+  if (clickedBtn.id === "password-1") {
+    const password2El = document.querySelector("#password-2");
+    password2El.textContent = password2;
+    password2El.innerHTML += copyIcon;
+  } else {
+    const password1El = document.querySelector("#password-1");
+    password1El.textContent = password1;
+    password1El.innerHTML += copyIcon;
+  }
 };
 
 // Set's password btns to default styling
